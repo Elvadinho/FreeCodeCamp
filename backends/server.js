0cbb8d7cae0bcfa4b057f058c13e541a954c78e3
ANANTIA TEMGOUA <elvado.temgoua@gmail.com>
2025-11-07 06:09:57 +0100

    adding a fake backend to put us in context

--- backends/server.js
+++ backends/server.js
@@ -1,1 +1,14 @@
-import http from "http";
+import http from "node:http";
+import { getDataFromDB } from "./database.js";
+
+const PORT = 8000;
+
+const server = http.createServer(async (req, res) => {
+  const destinations = await getDataFromDB();
+
+  if (req.url === "/api" && req.method === "GET") {
+    res.end(JSON.stringify(destinations));
+  }
+});
+
+server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
