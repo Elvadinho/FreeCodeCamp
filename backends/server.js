b1062b0a58875f607bb99d37d3eef088804d43f7
ANANTIA TEMGOUA <elvado.temgoua@gmail.com>
2025-11-12 12:00:47 +0100

    added the continent and api route

--- backends/server.js
+++ backends/server.js
@@ -1,5 +1,6 @@
 import http from "node:http";
 import { getDataFromDB } from "./database.js";
+import { sendJSON } from "./utils/sendJSON.js";
 
 const PORT = 8000;
 
@@ -7,8 +8,28 @@ const server = http.createServer(async (req, res) => {
   const destinations = await getDataFromDB();
 
   if (req.url === "/api" && req.method === "GET") {
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify(destinations));
+    sendJSON(res, 200, destinations);
+  } else if (req.url.startsWith("/api/continent") && req.method == "GET") {
+    const continent = req.url.split("/").pop();
+
+    let location = destinations.filter(
+      (e) => e.continent.toLowerCase() === `${continent.toLowerCase()}`
+    );
+
+    sendJSON(res, 200, location);
+  } else if (req.url.startsWith("/api/country") && req.method == "GET") {
+    const country = req.url.split("/").pop();
+
+    let location = destinations.filter(
+      (e) => e.country.toLowerCase() === `${country.toLowerCase()}`
+    );
+
+    sendJSON(res, 200, location);
+  } else {
+    sendJSON(res, 404, {
+      error: "not found",
+      message: "the requested route does not exist",
+    });
   }
 });
 
