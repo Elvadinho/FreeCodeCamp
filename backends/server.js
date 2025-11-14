859600b157cf311a9da167ae99a32710f0dc6dc4
ANANTIA TEMGOUA <elvado.temgoua@gmail.com>
2025-11-14 07:22:06 +0100

    Allows CORS and make possible to do queryParams

--- backends/server.js
+++ backends/server.js
@@ -2,14 +2,23 @@ import http from "node:http";
 import { getDataFromDB } from "./database.js";
 import { sendJSON } from "./utils/sendJSON.js";
 import { getData } from "./utils/getDataByPathParams.js";
+import { getDataByQueryParams } from "./utils/getDataByQueryParams.js";
 
 const PORT = 8000;
 
 const server = http.createServer(async (req, res) => {
   const destinations = await getDataFromDB();
 
-  if (req.url === "/api" && req.method === "GET") {
-    sendJSON(res, 200, destinations);
+  const urlObj = new URL(req.url, `http://${req.headers.host}`);
+
+  const queryObj = Object.fromEntries(urlObj.searchParams);
+
+  if (urlObj.pathname === "/api" && req.method === "GET") {
+    let filteredData = getDataByQueryParams(destinations, queryObj);
+
+    console.log(queryObj);
+
+    sendJSON(res, 200, filteredData);
   } else if (req.url.startsWith("/api/continent") && req.method == "GET") {
     const continent = req.url.split("/").pop();
 
--- backends/utils/sendJSON.js
+++ backends/utils/sendJSON.js
@@ -1,5 +1,7 @@
 export const sendJSON = (res, statusCode, payload) => {
   res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "application/json");
   res.statusCode = statusCode;
   res.end(JSON.stringify(payload));
 };
